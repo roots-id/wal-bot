854c962b2591747a865b82e681e3da88f1314720
Essbante <esteban.garcia@gmail.com>
2022-10-21 00:40:56 -0600

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -10,8 +10,9 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 #client = discord.Client(intents=discord.Intents.default())
-intents = discord.Intents(messages=True, guilds=True)
-bot = commands.Bot(command_prefix='!', intents=intents)
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 #@client.event
 @bot.event
@@ -25,15 +26,20 @@ async def on_ready():
 #@client.event
 @bot.event
 async def on_message(message):
+    print(message.channel.type)
     if message.author == bot.user:
         return
-    if message.content == 'yes':
-        await message.channel.send("cool")
-    print(message)
-    print(message.content)
+    # Direct message to bot
+    elif message.channel.type == discord.ChannelType.private:
+        await message.channel.send("I'm a bot, I don't respond to private messages")
+    # Server message with mention to bot
+    elif message.mentions and message.mentions[0] == bot.user:
+        await message.author.send("Type /help for a list of commands")
+    # INCLUDES THE COMMANDS FOR THE BOT. WITHOUT THIS LINE, YOU CANNOT TRIGGER YOUR COMMANDS.
+    await bot.process_commands(message)
 
 @bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx):
+async def nine(ctx):
     brooklyn_99_quotes = [
         'I\'m the human form of the 💯 emoji.',
         'Bingpot!',
